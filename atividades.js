62d73f193a5bf12959e811d077c110a8be4b06ad
Natanmlbb9900 <francisconatanal421@gmail.com>
2025-04-23 16:50:45 -0300

    oferta completa

---
+++ atividades.js
@@ -0,0 +1,80 @@
+// Questão 1 
+//let idadeMinima = 17;
+
+// if(idadeMinima >= 18){
+   // console.log("Maior de idade");
+// }else{
+   // console.log("Menor de idade");
+// }
+
+// Questão 2 
+
+// let numero = 10;
+
+// if(numero > 0){
+   // console.log("Positivo");
+// } else if(numero == 0){
+   // console.log("Zero");
+// } else {
+   // console.log("Negativo")
+// }
+
+// Questão 3 
+
+// let numeroA = 7;
+// let numeroB = 5;
+
+// if(numeroA > numeroB){
+   // console.log(`${numeroA} é maior que ${numeroB}`);
+// } else if(numeroB > numeroA){
+   // console.log(`${numeroB} é maior que ${numeroA}`);
+// } else //{
+    // console.log("Os números são iguais");
+// }
+
+// Questão 4 
+
+// let numero = 44;
+
+// if((numero % 2) == 0){
+    // console.log(`${numero} é par!`);
+// }else{
+   // console.log(`${numero} é impar!`);
+// }
+
+// Questão 5 
+
+// let notaA = 7;
+// let notaB = 7;
+// let notaC = 5;
+// let media = ((notaA + notaB + notaC) / 3).toFixed(1);
+// let situacao = "";
+
+// if (media >= 7) {
+//     situacao = "Aprovado";
+// }else if (media > 5 && media < 7) {
+//     situacao = "Recuperação";
+// }else{
+//     situacao = "Reprovado";
+// }
+
+// console.log(`${situacao}, média: ${media}`);
+
+// Questão 6 
+
+// let valor = 620;
+// let desconto = 0.35;
+// if(valor > 100){
+    
+//     console.log(`Total a pagar: R$ ${(valor - (valor * desconto)).toFixed(2)}`);
+    
+// }else{
+//     console.log(`Total a pagar: R$ ${valor.toFixed(2)}`);
+    
+// }
+
+// Questão 7
+
+let ano = 2020;
+
+if()
---
+++ index.js
@@ -0,0 +1,53 @@
+// let NomeSobrenome = "FranciscoLima";
+// let idade = 21;
+// console.log(idade)
+
+// // idade - idade + 1;
+// idade += 1;
+// console.log(idade);
+
+// let numero = 2;
+// // numero = numero 2;
+// numero *= 2 * 2;
+// console.log(numero);
+
+// console.log("2 == 2: ", 2 == 2);
+// console.log("true == true: ", false == false);
+// console.log("Francisco == Francisco: ", "Francisco == Francisco");
+// console.log("2 == 3: ", 2 == 3);
+// console.log("2 != 2: ", 2 != 2);
+// console.log("2 != 3: ", 2 != 3);
+
+// let idadeAlistamento = 18;
+// let minhaidade = 21;
+// console.log(minhaidade >= idadeAlistamento);
+// console.log(idadeAlistamento <= minhaidade)
+
+// console.log("2 == 2: ", 2 == 2);
+// console.log(" 2 == 2: ", 2 == "2");
+// console.log("2 === 2: ", 2 === 2);
+// console.log("2 === 2: ", 2 === "2");
+// console.log("2 != 2: ", 2 != 2);
+// console.log("2 != 2: ", 2 != "2");
+// console.log("2 !== 2: ", 2 !== 2);
+// console.log("2 !== 2: ", 2 !== "2");
+
+// let numero = 4;
+
+// console.log("2 == 2: ", numero == 2 && numero % 2 == 0);
+// console.log("2 == 2: ", numero == 2 || numero % 2 == 0);
+
+// let verdadeiro = true;
+// console.log(verdadeiro);
+// console.log(!verdadeiro);
+// verdadeiro = !verdadeiro;
+// console.log(!verdadeiro);
+
+
+let idadeMinima = 17;
+
+if(idadeMinima >= 17){
+    confirm.log("É de maior");
+}else{
+    console.log("E de menor");
+}
